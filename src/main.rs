3d477338c2f1c9d293ef119315b57b3a547351e0
Hana <53426441+hanako-eo@users.noreply.github.com>
2024-01-27 17:07:53 +0100

    Refactor with idiomatic code (#1)
    * Refactor with idiomatic code
    
    - remove all unwrap (I only keep the unwrap with the config)
    - remove a lot of ref and use if it's possible the owned value
    - reduce the number of mut if possible (to avoid side effects as much as possible)
    - keep `hashes.insert` in `download_checksums`
    - usage of more iterators
    
    * Format the file and take only &str for parse_response

--- src/main.rs
+++ src/main.rs
@@ -20,12 +20,12 @@ struct AppConfig {
 impl Default for AppConfig {
     fn default() -> Self {
         Self {
-            listen_address: "0.0.0.0".to_owned(),
+            listen_address: "0.0.0.0".to_string(),
             listen_port: 14770,
-            repo_owner: "DigitalpulseSoftware".to_owned(),
-            game_repository: "ThisSpaceOfMine".to_owned(),
-            updater_filename: "this_updater_of_mine".to_owned(),
-            updater_repository: "ThisUpdaterOfMine".to_owned(),
+            repo_owner: "DigitalpulseSoftware".to_string(),
+            game_repository: "ThisSpaceOfMine".to_string(),
+            updater_filename: "this_updater_of_mine".to_string(),
+            updater_repository: "ThisUpdaterOfMine".to_string(),
         }
     }
 }
@@ -52,162 +52,166 @@ struct VersionQuery {
 fn parse_response(asset_name: &str, response: &str) -> Option<String> {
     let parts: Vec<_> = response.split_whitespace().collect();
     if parts.len() != 2 {
-        println!(
+        eprintln!(
             "unexpected part count (expected sha256+filename, got {} parts)",
             parts.len()
         );
         return None;
     }
 
-    let sha256 = parts[0];
-    let filename = parts[1];
-    if !filename.starts_with('*') || &filename[1..] != asset_name {
-        println!("checksum file has wrong filename (expected *{asset_name} got {filename})");
-        return None;
+    let (sha256, filename) = (parts[0], parts[1]);
+    match !filename.starts_with('*') || &filename[1..] != asset_name {
+        false => Some(sha256.to_string()),
+        true => {
+            eprintln!("checksum file has wrong filename (expected *{asset_name} got {filename})");
+            None
+        }
     }
-
-    Some(sha256.to_string())
 }
 
 async fn download_sha256(client: &reqwest::Client, url: &Url) -> Result<String, reqwest::Error> {
     client.get(url.clone()).send().await?.text().await
 }
 
-async fn download_checksum<'a>(
+async fn download_checksum(
     client: &reqwest::Client,
-    entry_name: &'a str,
+    entry_name: &str,
     asset: &repos::Asset,
-    hashes: &mut HashMap<&'a str, String>,
     hashes_assets: &HashMap<&str, &repos::Asset>,
-) {
-    let checksum: Option<String> = match hashes_assets.get(entry_name) {
-        Some(entry) => match download_sha256(client, &entry.browser_download_url).await {
-            Ok(response) => parse_response(&asset.name, &response),
-            Err(err) => {
-                println!("failed to download sha256 of {entry_name}: {err}");
-                None
-            }
-        },
-        None => {
-            println!("{entry_name} hash not found");
-            None
-        }
+) -> Option<String> {
+    let Some(entry) = hashes_assets.get(entry_name) else {
+        eprintln!("{entry_name} hash not found");
+        return None;
     };
 
-    if let Some(hash) = checksum {
-        hashes.insert(entry_name, hash);
+    match download_sha256(client, &entry.browser_download_url).await {
+        Ok(response) => parse_response(asset.name.as_str(), response.as_str()),
+        Err(err) => {
+            eprintln!("failed to download sha256 of {entry_name}: {err}");
+            None
+        }
     }
 }
 
-async fn download_checksums<'a>(
+async fn download_checksums<'a, 'b, B>(
     assets_asset: Option<&repos::Asset>,
-    binary_assets: &HashMap<&'a str, &repos::Asset>,
-    hashes_assets: &HashMap<&str, &repos::Asset>,
-) -> HashMap<&'a str, String> {
+    binary_assets: B,
+    hashes_assets: HashMap<&'b str, &repos::Asset>,
+) -> HashMap<&'b str, String>
+where
+    'b: 'a,
+    B: Iterator<Item = (&'a &'b str, &'a &'b repos::Asset)>,
+{
     let client = reqwest::Client::new();
 
     let mut hashes = HashMap::new();
     for (entry_name, asset) in binary_assets {
-        download_checksum(&client, entry_name, asset, &mut hashes, hashes_assets).await;
+        if let Some(checksum) = download_checksum(&client, entry_name, asset, &hashes_assets).await
+        {
+            hashes.insert(*entry_name, checksum);
+        }
     }
 
     if let Some(asset) = assets_asset {
-        download_checksum(&client, "assets", asset, &mut hashes, hashes_assets).await;
+        if let Some(checksum) = download_checksum(&client, "assets", asset, &hashes_assets).await {
+            hashes.insert("assets", checksum);
+        }
     }
 
     hashes
 }
 
 fn remove_game_suffix(asset_name: &str) -> &str {
-    let mut platform = asset_name;
-    platform = platform.find('.').map_or(platform, |pos| &platform[..pos]);
-    platform = platform
-        .find("_releasedbg")
-        .map_or(platform, |pos| &platform[..pos]);
-
+    let platform = asset_name
+        .find('.')
+        .map_or(asset_name, |pos| &asset_name[..pos]);
     platform
+        .find("_releasedbg")
+        .map_or(platform, |pos| &platform[..pos])
 }
 
 async fn get_updater_releases(
     app_config: &AppConfig,
 ) -> Option<HashMap<String, DownloadableAsset>> {
-    let octo = octocrab::instance();
-    let output = octo
+    let output = octocrab::instance()
         .repos(&app_config.repo_owner, &app_config.updater_repository)
         .releases()
         .list()
         .send()
         .await;
 
-    if output.is_err() {
-        println!(
-            "failed to retrieve auto-updater releases: {}",
-            output.unwrap_err()
-        );
-        return None;
-    }
-    let releases = output.unwrap();
-
-    let last_release = releases.into_iter().find(|release| !release.prerelease);
-
-    if last_release.is_none() {
-        println!("no auto-updater releases found");
-        return None;
-    }
-    let last_release = last_release.unwrap();
-
-    let mut binary_assets = HashMap::new();
-    let mut hashes_assets = HashMap::new();
-
-    for asset in &last_release.assets {
-        let platform = remove_game_suffix(&asset.name);
-        if asset.name.ends_with(".sha256") {
-            hashes_assets.insert(platform, asset);
-        } else {
-            binary_assets.insert(platform, asset);
+    let releases = match output {
+        Ok(output) => output,
+        Err(err) => {
+            eprintln!("failed to retrieve auto-updater releases: {}", err);
+            return None;
         }
-    }
+    };
 
-    let hashes = download_checksums(None, &binary_assets, &hashes_assets).await;
+    let Some(last_release) = releases.into_iter().find(|release| !release.prerelease) else {
+        eprintln!("no auto-updater releases found");
+        return None;
+    };
 
-    let mut binaries = HashMap::new();
-    for (platform, asset) in binary_assets {
-        binaries.insert(
-            platform.to_string(),
-            DownloadableAsset {
-                download_url: asset.browser_download_url.to_string(),
-                size: asset.size,
-                sha256: hashes.get(platform).map(String::clone),
-            },
-        );
-    }
+    let (binary_assets, hashes_assets) = last_release.assets.iter().fold(
+        (HashMap::new(), HashMap::new()),
+        |(mut binary_assets, mut hashes_assets), asset| {
+            let platform = remove_game_suffix(asset.name.as_str());
+
+            let container_assets = match asset.name.ends_with(".sha256") {
+                true => &mut hashes_assets,
+                false => &mut binary_assets,
+            };
+
+            container_assets.insert(platform, asset);
+
+            (binary_assets, hashes_assets)
+        },
+    );
+
+    let hashes = download_checksums(None, binary_assets.iter(), hashes_assets).await;
+
+    let binaries = binary_assets
+        .into_iter()
+        .map(|(platform, asset)| {
+            (
+                platform.to_string(),
+                DownloadableAsset {
+                    download_url: asset.browser_download_url.to_string(),
+                    size: asset.size,
+                    sha256: hashes.get(platform).cloned(),
+                },
+            )
+        })
+        .collect::<HashMap<String, DownloadableAsset>>();
 
     Some(binaries)
 }
 
 #[derive(Serialize)]
-struct GameVersion<'a> {
-    assets: &'a DownloadableAsset,
+struct GameVersion {
+    assets: DownloadableAsset,
     assets_version: String,
-    binaries: &'a DownloadableAsset,
-    updater: &'a DownloadableAsset,
+    binaries: DownloadableAsset,
+    updater: DownloadableAsset,
     version: String,
 }
 
 async fn get_game_releases(app_config: &AppConfig) -> Option<Vec<GameRelease>> {
-    let octo = octocrab::instance();
-    let output = octo
+    let output = octocrab::instance()
         .repos(&app_config.repo_owner, &app_config.game_repository)
         .releases()
         .list()
         .send()
         .await;
 
-    if output.is_err() {
-        println!("failed to retrieve game releases: {}", output.unwrap_err());
-        return None;
-    }
-    let releases = output.unwrap();
+    let releases = match output {
+        Ok(output) => output,
+        Err(err) => {
+            eprintln!("failed to retrieve game releases: {}", err);
+            return None;
+        }
+    };
 
     let mut assets_info = None;
     let mut assets_version = None;
@@ -217,10 +221,11 @@ async fn get_game_releases(app_config: &AppConfig) -> Option<Vec<GameRelease>> {
         if release.prerelease {
             continue;
         }
+
         let version = match Version::parse(&release.tag_name) {
             Ok(version) => version,
             Err(err) => {
-                println!(
+                eprintln!(
                     "failed to parse version of tag {0}: {1}",
                     release.tag_name, err
                 );
@@ -228,56 +233,59 @@ async fn get_game_releases(app_config: &AppConfig) -> Option<Vec<GameRelease>> {
             }
         };
 
-        let mut binary_assets = HashMap::new();
-        let mut hashes_assets = HashMap::new();
+        let (binary_assets, hashes_assets, assets_asset) = release.assets.iter().fold(
+            (HashMap::new(), HashMap::new(), None),
+            |(mut binary_assets, mut hashes_assets, assets_asset), asset| {
+                let platform = remove_game_suffix(asset.name.as_str());
 
-        let mut assets_asset = None;
+                if asset.name.ends_with(".sha256") {
+                    hashes_assets.insert(platform, asset);
+                } else if platform == "assets" {
+                    return (binary_assets, hashes_assets, Some(asset));
+                } else {
+                    binary_assets.insert(platform, asset);
+                }
 
-        for asset in &release.assets {
-            let platform = remove_game_suffix(&asset.name);
-            if asset.name.ends_with(".sha256") {
-                hashes_assets.insert(platform, asset);
-            } else if platform == "assets" {
-                assets_asset = Some(asset);
-            } else {
-                binary_assets.insert(platform, asset);
-            }
-        }
+                (binary_assets, hashes_assets, assets_asset)
+            },
+        );
 
-        let hashes = download_checksums(assets_asset, &binary_assets, &hashes_assets).await;
+        let hashes = download_checksums(assets_asset, binary_assets.iter(), hashes_assets).await;
 
         // Build assets
         if let Some(asset) = assets_asset {
             assets_info = Some(DownloadableAsset {
                 download_url: asset.browser_download_url.to_string(),
                 size: asset.size,
-                sha256: hashes.get("assets").map(String::clone),
+                sha256: hashes.get("assets").cloned(),
             });
             assets_version = Some(version.clone());
         }
 
         // Build binaries
-        let mut binaries = HashMap::new();
-        for (platform, asset) in binary_assets {
-            binaries.insert(
-                platform.to_string(),
-                DownloadableAsset {
-                    download_url: asset.browser_download_url.to_string(),
-                    size: asset.size,
-                    sha256: hashes.get(platform).map(String::clone),
-                },
-            );
-        }
+        let binaries = binary_assets
+            .into_iter()
+            .map(|(platform, asset)| {
+                (
+                    platform.to_string(),
+                    DownloadableAsset {
+                        download_url: asset.browser_download_url.to_string(),
+                        size: asset.size,
+                        sha256: hashes.get(platform).cloned(),
+                    },
+                )
+            })
+            .collect::<HashMap<String, DownloadableAsset>>();
 
-        // if we don't have assets URL/versions yet we must skip
-        if assets_info.is_none() || assets_version.is_none() {
-            println!("ignoring release {version} because no assets was found");
+        let (Some(assets_info), Some(assets_version)) = (&assets_info, &assets_version) else {
+            // if we don't have assets URL/versions yet we must skip
+            eprintln!("ignoring release {version} because no assets was found");
             continue;
-        }
+        };
 
         game_releases.push(GameRelease {
-            assets: assets_info.as_ref().unwrap().clone(),
-            assets_version: assets_version.as_ref().unwrap().clone(),
+            assets: assets_info.clone(),
+            assets_version: assets_version.clone(),
             binaries,
             version,
         });
@@ -291,44 +299,40 @@ async fn game_version(
     app_config: web::Data<AppConfig>,
     ver_query: web::Query<VersionQuery>,
 ) -> impl Responder {
-    let updater_releases = match get_updater_releases(&app_config).await {
-        Some(release) => release,
-        None => {
-            return HttpResponse::InternalServerError().finish();
-        }
+    let Some(updater_releases) = get_updater_releases(&app_config).await else {
+        return HttpResponse::InternalServerError().finish();
     };
 
-    match get_game_releases(&app_config).await {
-        Some(game_releases) => {
-            for release in game_releases.iter().rev() {
-                let updater_filename =
-                    format!("{}_{}", &ver_query.platform, app_config.updater_filename);
-                let updater_release = match updater_releases.get(&updater_filename) {
-                    Some(release) => release,
-                    None => {
-                        println!(
-                            "no updater release found for platform {}",
-                            ver_query.platform
-                        );
-                        return HttpResponse::InternalServerError().finish();
-                    }
-                };
+    let Some(game_releases) = get_game_releases(&app_config).await else {
+        return HttpResponse::InternalServerError().finish();
+    };
 
-                if let Some(binaries) = release.binaries.get(&ver_query.platform) {
-                    let reponse = GameVersion {
-                        assets: &release.assets,
-                        assets_version: release.assets_version.to_string(),
-                        binaries,
-                        updater: updater_release,
-                        version: release.version.to_string(),
-                    };
-                    return HttpResponse::Ok().json(web::Json(reponse));
-                }
-            }
+    let game_release = game_releases.into_iter().rev().find_map(|release| {
+        let updater_filename = format!("{}_{}", &ver_query.platform, app_config.updater_filename);
 
+        let updater_release = updater_releases.get(&updater_filename)?;
+
+        release
+            .binaries
+            .get(&ver_query.platform)
+            .map(|binaries| GameVersion {
+                assets: release.assets,
+                assets_version: release.assets_version.to_string(),
+                binaries: binaries.clone(),
+                updater: updater_release.clone(),
+                version: release.version.to_string(),
+            })
+    });
+
+    match game_release {
+        Some(response) => HttpResponse::Ok().json(web::Json(response)),
+        None => {
+            eprintln!(
+                "no updater release found for platform {}",
+                ver_query.platform
+            );
             HttpResponse::NotFound().finish()
         }
-        None => HttpResponse::InternalServerError().finish(),
     }
 }
 
@@ -339,12 +343,13 @@ async fn main() -> Result<(), std::io::Error> {
     std::env::set_var("RUST_LOG", "info,actix_web=info");
     env_logger::init();
 
-    let bind_address = format!("{}:{}", &config.listen_address, &config.listen_port);
+    let bind_address = format!("{}:{}", config.listen_address, config.listen_port);
 
+    let data_config = web::Data::new(config);
     HttpServer::new(move || {
         App::new()
             .wrap(middleware::Logger::default())
-            .app_data(web::Data::new(config.clone()))
+            .app_data(data_config.clone())
             .service(game_version)
     })
     .bind(bind_address)?
