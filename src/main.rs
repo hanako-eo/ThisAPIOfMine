6512404ffe97a303b2b0d5dd0c32b8669d1195b8
SirLynix <lynix680@gmail.com>
2024-06-26 22:20:59 +0200

    Move game_version route to its own file

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,13 @@
-use std::collections::HashMap;
-use std::sync::Mutex;
-
-use actix_web::{get, middleware, web, App, HttpServer};
-use actix_web::{HttpResponse, Responder};
-use cached::{CachedAsync, TimedCache};
+use actix_web::{middleware, web, App, HttpServer};
+use cached::TimedCache;
 use confy::ConfyError;
-use game_data::{Asset, GameRelease};
-use serde::Deserialize;
+use std::sync::Mutex;
 
 use crate::app_data::AppData;
 use crate::config::ApiConfig;
 use crate::fetcher::Fetcher;
-use crate::game_data::GameVersion;
-use crate::players::create_player;
+use crate::players::{player_authenticate, player_create};
+use crate::version::game_version;
 
 mod app_data;
 mod config;
@@ -20,81 +15,9 @@ mod errors;
 mod fetcher;
 mod game_data;
 mod players;
+mod version;
 
 use tokio_postgres::NoTls;
-
-#[derive(Deserialize)]
-struct VersionQuery {
-    platform: String,
-}
-
-#[derive(Clone)]
-enum CachedReleased {
-    Updater(HashMap<String, Asset>),
-    Game(GameRelease),
-}
-
-#[get("/game_version")]
-async fn game_version(
-    app_data: web::Data<AppData>,
-    ver_query: web::Query<VersionQuery>,
-) -> impl Responder {
-    let AppData {
-        cache,
-        config,
-        fetcher,
-    } = app_data.as_ref();
-    let mut cache = cache.lock().unwrap();
-
-    // TODO: remove .cloned
-    let Ok(CachedReleased::Updater(updater_release)) = cache
-        .try_get_or_set_with("latest_updater_release", || async {
-            fetcher
-                .get_latest_updater_release()
-                .await
-                .map(CachedReleased::Updater)
-        })
-        .await
-        .cloned()
-    else {
-        return HttpResponse::InternalServerError().finish();
-    };
-
-    // TODO: remove .cloned
-    let Ok(CachedReleased::Game(game_release)) = cache
-        .try_get_or_set_with("latest_game_release", || async {
-            fetcher
-                .get_latest_game_release()
-                .await
-                .map(CachedReleased::Game)
-        })
-        .await
-        .cloned()
-    else {
-        return HttpResponse::InternalServerError().finish();
-    };
-
-    let updater_filename = format!("{}_{}", ver_query.platform, config.updater_filename);
-
-    let (Some(updater), Some(binary)) = (
-        updater_release.get(&updater_filename),
-        game_release.binaries.get(&ver_query.platform),
-    ) else {
-        eprintln!(
-            "no updater or game binary release found for platform {}",
-            ver_query.platform
-        );
-        return HttpResponse::NotFound().finish();
-    };
-
-    HttpResponse::Ok().json(web::Json(GameVersion {
-        assets: game_release.assets,
-        assets_version: game_release.assets_version.to_string(),
-        binaries: binary.clone(),
-        updater: updater.clone(),
-        version: game_release.version.to_string(),
-    }))
-}
 
 fn setup_pg_pool(api_config: &ApiConfig) -> deadpool_postgres::Pool {
     use deadpool_postgres::{Config, ManagerConfig, RecyclingMethod, Runtime};
@@ -152,7 +75,7 @@ async fn main() -> Result<(), std::io::Error> {
             .app_data(data_config.clone())
             .app_data(pg_pool.clone())
             .service(game_version)
-            .service(create_player)
+            .service(player_create)
     })
     .bind(bind_address)?
     .run()
---
+++ src/version.rs
@@ -0,0 +1,82 @@
+use std::collections::HashMap;
+
+use actix_web::{get, web};
+use actix_web::{HttpResponse, Responder};
+use cached::CachedAsync;
+use serde::Deserialize;
+
+use crate::app_data::AppData;
+use crate::game_data::{Asset, GameRelease, GameVersion};
+
+#[derive(Deserialize)]
+struct VersionQuery {
+    platform: String,
+}
+
+#[derive(Clone)]
+pub(crate) enum CachedReleased {
+    Updater(HashMap<String, Asset>),
+    Game(GameRelease),
+}
+
+#[get("/game_version")]
+async fn game_version(
+    app_data: web::Data<AppData>,
+    ver_query: web::Query<VersionQuery>,
+) -> impl Responder {
+    let AppData {
+        cache,
+        config,
+        fetcher,
+    } = app_data.as_ref();
+    let mut cache = cache.lock().unwrap();
+
+    // TODO: remove .cloned
+    let Ok(CachedReleased::Updater(updater_release)) = cache
+        .try_get_or_set_with("latest_updater_release", || async {
+            fetcher
+                .get_latest_updater_release()
+                .await
+                .map(CachedReleased::Updater)
+        })
+        .await
+        .cloned()
+    else {
+        return HttpResponse::InternalServerError().finish();
+    };
+
+    // TODO: remove .cloned
+    let Ok(CachedReleased::Game(game_release)) = cache
+        .try_get_or_set_with("latest_game_release", || async {
+            fetcher
+                .get_latest_game_release()
+                .await
+                .map(CachedReleased::Game)
+        })
+        .await
+        .cloned()
+    else {
+        return HttpResponse::InternalServerError().finish();
+    };
+
+    let updater_filename = format!("{}_{}", ver_query.platform, config.updater_filename);
+
+    let (Some(updater), Some(binary)) = (
+        updater_release.get(&updater_filename),
+        game_release.binaries.get(&ver_query.platform),
+    ) else {
+        eprintln!(
+            "no updater or game binary release found for platform {}",
+            ver_query.platform
+        );
+        return HttpResponse::NotFound().finish();
+    };
+
+    HttpResponse::Ok().json(web::Json(GameVersion {
+        assets: game_release.assets,
+        assets_version: game_release.assets_version.to_string(),
+        binaries: binary.clone(),
+        updater: updater.clone(),
+        version: game_release.version.to_string(),
+    }))
+}
